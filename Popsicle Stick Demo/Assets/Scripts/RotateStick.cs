49d143294123a2b65165caebe6b4e6cb08f5337b
israfiluzuk <israfiluzuk@hotmail.com>
2020-03-14 16:28:30 +0300

    rastgele açı işlemi
    işlem düzeltildi.

---
+++ Popsicle Stick Demo/Assets/Scripts/RotateStick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateStick : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           // transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + 45));
+            transform.localRotation = Quaternion.EulerAngles(0,0,transform.localEulerAngles.z - 45);
+        }
+    }
+
+}
--- Popsicle Stick Demo/Assets/Scripts/StickSpawner.cs
+++ Popsicle Stick Demo/Assets/Scripts/StickSpawner.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
 
 public class StickSpawner : MonoBehaviour
 {
-
+    
     public GameObject Stick;
     int[] degrees = { 0, 45, 90, 135 };
     int value;
@@ -12,34 +12,61 @@ public class StickSpawner : MonoBehaviour
     public Color[] stickColors;
     public Node[] nodes;
     public Node selectableNode;
+
+    public List<int> degreeList = new List<int>();
+
+
+    int maxNumbers = 4;
+
     void Start()
     {
-
+        DegreeListAdder();
+    }
+    void DegreeListAdder()
+    {
+        degreeList.Add(0);
+        degreeList.Add(45);
+        degreeList.Add(90);
+        degreeList.Add(135);
     }
 
     public void ReGenerate()
-    {
+    { 
 
+        selectableNode.sticks.Clear();
         for (int i = 0; i < sticks.childCount; i++)
         {
             Destroy(sticks.GetChild(i).gameObject);
         }
-        selectableNode.sticks.Clear();
+        degreeList.Clear();
+        DegreeListAdder();
+
 
         for (int i = 1; i < Random.Range(1f, 4f); i++)
         {
-            Debug.Log("Rand : " + i);
-           
-            value = (int)Random.Range(0f, 4f);
-            Debug.Log(degrees[value]);
+            
+           //Debug.Log("Rand : " + i);
+
+            value = Random.Range(0,degreeList.Count);
+
+
             //Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-            GameObject stickInit = Instantiate(Stick, transform.position, Quaternion.Euler(0, 0, degrees[value]), sticks);
+            GameObject stickInit = Instantiate(Stick, transform.position, Quaternion.Euler(0, 0, (-1)*degreeList[value]), sticks);
+
+            Debug.Log("__"+ degreeList[value]);
+            
+            
             int colorId = Random.Range(0, stickColors.Length);
             stickInit.GetComponent<Renderer>().material.color = stickColors[colorId];
-            stickInit.GetComponent<Stick>().degree = degrees[value];
+            stickInit.GetComponent<Stick>().degree = degreeList[value];
+            degreeList.RemoveAt(value);Debug.Log("UZUNLUK : " + degreeList.Count);
             stickInit.GetComponent<Stick>().color = stickColors[colorId];
             selectableNode.sticks.Add(stickInit.GetComponent<Stick>());
+
+
         }
+
+       // degreeList = degreeList.OrderBy(tvz => System.Guid.NewGuid()).ToList();
     }
 
     public void MoveStick()
